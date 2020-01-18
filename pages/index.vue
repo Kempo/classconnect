c9b7ee8b1451126ca5519e3a66c5dab50e698e05
Aaron <aaronc@timpli.com>
2020-01-18 12:57:46 -0800

    aaron commit

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         classconnect
       </h1>
       <h2 class="subtitle">
-        My best Nuxt.js project
+        Aaron Chen
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
