337b59657d0bb252c907e6743570b8af0e04ffdf
kevylees <kevylees@uw.edu>
2020-01-18 18:28:33 -0800

    kevin commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,48 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        classconnect - hello
-      </h1>
+      <img src="~/assets/Group 3.svg" class="logo" height="100" alt="logo" />
       <h2 class="subtitle">
-        Aaron Chen
+        Connect Schools.Connect Classrooms.Connect Students.
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    </div>
+    <div>
+      <b-button variant="success" class="btn" href="/explore">Connect</b-button>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
+<script></script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+body {
+  font-family: Product Sans Medium, sans-serif;
+  background-color: #e5e5e;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.logo {
+  animation: fadeIn ease 3s;
+  margin-left: 315px;
+  margin-top: 175px;
+  margin-bottom: 50px;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  animation: fadeIn ease 7s;
+  margin-left: 160px;
 }
 
-.links {
-  padding-top: 15px;
+.btn {
+  margin-left: 510px;
+  margin-top: 30px;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
